7fc78f1043e515f4999a13859dcbb4639f702746
hanxue <hanxue@cmhi.chinamobile.com>
2023-08-03 19:23:38 +0800

    Opt 提交一些代码

--- vite-project/src/router/routes.ts
+++ vite-project/src/router/routes.ts
@@ -1,13 +1,14 @@
 import { RouteRecordRaw } from 'vue-router';
 import Test from '../views/testAlert.vue';
 import TestDialog from '../views/testDialog.vue';
+import TesInputfrom '../views/testInput.vue';
 
 const routes: RouteRecordRaw[] = [
   // 其他路由配置
   {
     path: '/',
     name: 'TestDialog',
-    component: TestDialog,
+    component: TesInputfrom,
   },
 ];
 
---
+++ vite-project/src/views/testInput.vue
@@ -0,0 +1,10 @@
+<template>
+    <TInput v-model="values"></TInput>
+</template>
+
+<script setup lang="ts">
+import TInput from '../components/input/input'
+import { ref } from  'vue'
+
+const values = ref('iuiuiu')
+</script>
