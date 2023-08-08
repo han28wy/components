22a62f5340e5ea162388627971d30194ae771145
hanxue <hanxue@cmhi.chinamobile.com>
2023-08-08 21:54:33 +0800

    Opt 提交

--- vite-project/src/components/checkbox/checkbox.vue
+++ vite-project/src/components/checkbox/checkbox.vue
@@ -1,60 +1,65 @@
 <template>
-    <label class="el-checkbox" :class="{'is-bordered': props.border}">
-        <span>
-            <input v-model="model" :value="props.label" :checked="currentValue" type="checkbox" @change="handleChange"/>
-        </span>
-        <span>{{props.label}}</span>
-    </label>
+  <label class="el-checkbox" :class="{ 'is-bordered': props.border }">
+    <span>
+      <input
+        v-model="model"
+        :value="props.label"
+        :true-value="props.trueLabel"
+        :false-value="props.falseLabel"
+        :checked="currentValue"
+        type="checkbox"
+        @change="handleChange"
+      />
+    </span>
+    <span>{{ props.label }}</span>
+  </label>
 </template>
 
 <script lang="ts" setup>
-import {checkboxProps} from './checkbox'
-import { ref, computed } from 'vue';
+import { checkboxProps } from "./checkbox";
+import { ref, computed } from "vue";
 
 const props = defineProps(checkboxProps);
-const emits = defineEmits(['change']);
+const emits = defineEmits(["change"]);
 
-const model = computed(()=>{})
-const isChecked = ref(false)
+const model = computed(() => {});
 
 const getLabeledValue = (value: string | number | boolean) => {
-    return value === props.trueLabel || value === true
-      ? props.trueLabel ?? true
-      : props.falseLabel ?? false
- }
+  return value === props.trueLabel || value === true
+    ? props.trueLabel ?? true
+    : props.falseLabel ?? false;
+};
 
 const handleChange = (event: Event) => {
+  const checked = event.target.checked;
+  console.log("🚀 ~ file: checkbox.vue:30 ~ handleChange ~ checked:", checked);
+  currentValue.value = checked;
+  const value = checked ? true : false;
+  emits("update:modelValue", value);
+  emits("change", value, event);
+};
 
-    const checked = event.target.checked;
-    console.log("🚀 ~ file: checkbox.vue:30 ~ handleChange ~ checked:", checked)
-    currentValue.value = checked
-    const value = checked ? true : false
-    emits('update:modelValue', value)
-    const target = event.target as HTMLInputElement
-    emits('change', value, event)
-}
-
-const currentValue = ref(props.modelValue)
+const currentValue = ref(props.modelValue);
 </script>
 
 <style>
 .el-checkbox {
-    color: #606266;
-    font-weight: 500;
-    font-size: 14px;
-    position: relative;
-    cursor: pointer;
-    display: inline-block;
-    white-space: nowrap;
-    user-select: none;
-    margin-right: 30px;
+  color: #606266;
+  font-weight: 500;
+  font-size: 14px;
+  position: relative;
+  cursor: pointer;
+  display: inline-block;
+  white-space: nowrap;
+  user-select: none;
+  margin-right: 30px;
 }
-.is-bordered{
-    padding: 9px 20px 9px 10px;
-    border-radius: 4px;
-    border: 1px solid #dcdfe6;
-    box-sizing: border-box;
-    line-height: normal;
-    height: 40px;
+.is-bordered {
+  padding: 9px 20px 9px 10px;
+  border-radius: 4px;
+  border: 1px solid #dcdfe6;
+  box-sizing: border-box;
+  line-height: normal;
+  height: 40px;
 }
 </style>
--- vite-project/src/views/testCheckbox.vue
+++ vite-project/src/views/testCheckbox.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
         <span>checkboxGroup</span>
-        <!-- <CheckboxGroup v-model="checkboxGroup2" disabled>
-            <Checkbox label="备选项1" border @change="handleChange"></Checkbox>
+        <CheckboxGroup v-model="checkboxGroup2" @change="handleChangeGroup" disabled>
+            <Checkbox label="备选项1" border ></Checkbox>
             <Checkbox label="备选项2" border></Checkbox>
-        </CheckboxGroup> -->
-        <span>checkbox</span>
-        <Checkbox v-model="checked" label="beixuan" border @change="handleChange"></Checkbox>
+            <Checkbox label="备选项3" border></Checkbox>
+            <Checkbox label="备选项4" border></Checkbox>
+            <Checkbox label="备选项5" border></Checkbox>
+        </CheckboxGroup>
+        <!-- <span>checkbox</span>
+        <Checkbox v-model="checked" label="beixuan" border @change="handleChange"></Checkbox> -->
     </div>
 </template>
 
@@ -16,9 +19,13 @@ import Checkbox from '../components/checkbox/checkbox.vue'
 import { ref } from 'vue'
 
 const checked = ref(true)
-const checkboxGroup2 = ref(false)
+const checkboxGroup2 = ref(['备选项1','备选项3'])
 
 const handleChange = () =>{
     console.log('gggg   ', checked.value)
 }
+
+const handleChangeGroup(()=>{
+    console.log('fffff')
+})
 </script>
